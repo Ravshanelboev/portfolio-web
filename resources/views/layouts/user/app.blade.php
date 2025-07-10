445a8c811afa204db7fd95c32ba4dde17927e9c7
Ravshanelboev <ravshanelboyevv@gmail.com>
2025-07-11 02:29:59 +0500

    blade file template-user

---
+++ resources/views/layouts/user/app.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>@yield('title', 'Elboyevv')</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Favicons -->
+    <link href="{{ asset('user/assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('user/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto&family=Poppins&family=Raleway&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('user/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="{{ asset('user/assets/css/main.css') }}" rel="stylesheet">
+</head>
+
+<body class="@yield('body-class')">
+
+    @include('components.user.header')
+
+    <main class="main">
+        @yield('content')
+    </main>
+
+    @include('components.user.footer')
+
+    <!-- Scroll Top -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('user/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('user/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+
+    <!-- Main JS File -->
+    <script src="{{ asset('user/assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.user.app')
+
+@section('title', 'Home')
+@section('body-class', 'index-page')
+
+@section('content')
+
+<!-- Hero Section -->
+<section id="hero" class="hero section">
+  <img src="{{ asset('user/assets/img/hero-bg.jpg') }}" alt="" data-aos="fade-in">
+
+  <div class="container text-center" data-aos="zoom-out" data-aos-delay="100">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <h2>Kelly Adams</h2>
+        <p>I'm a professional illustrator from San Francisco</p>
+        <a href="{{ url('/about') }}" class="btn-get-started">About Me</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /Hero Section -->
+
+@endsection
